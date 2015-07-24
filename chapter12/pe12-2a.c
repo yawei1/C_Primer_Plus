8d4a2bd7a3448592749d1277488dd9b90661e635
Yawei Zhang <zyw.jsly@gmail.com>
2015-07-24 15:50:47 -0700

    crate the foler of chapter 12

---
+++ chapter12/pe12-2a.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "pe12-2a.h"
+
+int mode;
+float distance;
+float fuel;
+
+void set_mode(int input)
+{
+	if (input == 0) {
+		mode = 0; 
+	}
+	else if (input == 1) {
+		mode = 1;
+	}
+	else if (input > 1) {
+		printf("Invalid mode specified. Mode 1 (US) used.\n");
+		mode = 1;
+	}
+}
+
+void get_info() 
+{
+	if (mode == 0) {
+		printf ("Enter distance traveled in kilometers: ");
+		scanf ("%f", &distance);
+		printf ("Enter fuel consumed in liters: ");
+		scanf ("%f", &fuel);
+	}
+	if (mode == 1) {
+		printf ("Enter distnace traveled in miles: ");
+		scanf ("%f", &distance);
+		printf ("Enter fuel consumed in gallons: ");
+		scanf ("%f", &fuel);
+	}
+
+}
+
+void show_info()
+{
+	float lph;
+	float mpg;
+	if (mode == 0) {
+		lph = fuel / distance * 100;
+		printf ("Fuel consumption is %.2f liters per 100 km.\n", lph);
+	}
+	else if (mode == 1) {
+		mpg = distance / fuel;
+		printf ("Fuel consumption is %.1f miles per gallon.\n", mpg);
+	}
+}
---
+++ chapter12/pe12-2a.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void set_mode(int mode);
+
+void get_info();
+
+void show_info();
