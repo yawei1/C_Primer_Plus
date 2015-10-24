add719f2ac8c8c8154ff30d65f03d4c071b30853
Yawei Zhang <zhanyawe@flip3.engr.oregonstate.edu>
2015-10-24 03:21:48 -0700

    13.4.c

--- chapter13/13.4.c
+++ chapter13/13.4.c
@@ -7,19 +7,22 @@ int main(int argc, char *argv[])
 {
 	FILE *fp;
 	char line[MAXLINE];
+	int files = 0;
 
 	for (int i = 1; i < argc; i++) {
 		//show all files
 		if ((fp = fopen(argv[i], "r")) == NULL) {
 			fprintf(stderr, "Cannot open file %s.\n", argv[i]);
+			continue;
 		}
 		fprintf(stdout, "File %s: \n", argv[i]);
 		while(fgets(line, MAXLINE, fp) != NULL) {
 			fputs(line, stdout);
 		}
 		fclose(fp);
+		files++;
 	}
 
-	printf("%d files printed. Done.\n", argc);
+	printf("%d files printed. Done.\n", files);
 	return 0;
 }
